522a8973b9ac55ad922cc0fee4a76dc1281992cd
Gitanshu <gitanshugupta7@gmail.com>
2020-07-02 17:13:38 +0530

    added http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MainComponent
+    MainComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
